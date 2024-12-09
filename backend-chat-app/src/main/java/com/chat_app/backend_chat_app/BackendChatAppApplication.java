6b047469000789ca98ed5617f40a83ab22e3879e
cdpatra <cdpatra03@gmail.com>
2024-12-09 07:40:33 +0530

    setup the backend

---
+++ backend-chat-app/src/main/java/com/chat_app/backend_chat_app/BackendChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.chat_app.backend_chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChatAppApplication.class, args);
+	}
+
+}
